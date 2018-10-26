0e445ee425040bf66d99da862b0227a10dec19f1
Soutaro Matsumoto <matsumoto@soutaro.com>
2018-10-26 16:14:39 -0700

    Add typechecking example

---
+++ example/example.rb
@@ -0,0 +1,27 @@
+Schema = _ = StrongJSON.new do
+  # @type self: AddressSchema
+
+  let :address, object(address: string, country: symbol?)
+  let :email, object(email: string)
+  let :contact, enum(address, email)
+  let :person, object(name: string, contacts: array(contact))
+end
+
+person = Schema.person.coerce(nil)
+
+# @type var name: String
+name = person[:name]
+
+# @type var contacts: Array<email | address>
+contacts = person[:contacts]
+
+contacts.each do |contact|
+    case 
+    when contact.keys.include?(:email)
+        # @type var contact: email
+        puts "Email: #{contact[:email]}"
+    when contact.keys.include?(:address)
+        # @type var contact: address
+        puts "Address: #{contact[:address]} (#{contact[:country] || "unspecified"})"
+    end
+end
