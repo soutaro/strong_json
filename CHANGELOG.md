88687f42a13c01bbaa9373769befdcbf3391b13f
Soutaro Matsumoto <matsumoto@soutaro.com>
2018-10-29 13:42:57 -0700

    Version 0.8.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG
+
+## master
+
+## 0.8.0 (2018-10-29)
+
+* Add steep typechecking
--- lib/strong_json/version.rb
+++ lib/strong_json/version.rb
@@ -1,5 +1,5 @@
 class StrongJSON
   # @dynamic initialize, let
   
-  VERSION = "0.7.1"
+  VERSION = "0.8.0"
 end
